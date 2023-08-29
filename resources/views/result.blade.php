33dbf6b3da4cafffd40a2026515686c628e7c751
hirooooota <hirooooota@gmail.com>
2023-08-30 04:37:22 +0900

    計算アプリ課題完了

---
+++ resources/views/result.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=   , initial-scale=1.0">
+    <title>Result</title>
+</head>
+
+<body>
+    <h1>計算アプリ</h1>
+    <h2>{{ $result }}</h2>
+</body>
+
+</html>
