5d5d86792cee2ae2164bf2da85cdaa26c604c825
hirooooota <140122495+hirooooota@users.noreply.github.com>
2023-08-30 04:44:32 +0900

    Revert "計算アプリ課題完了"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CalcController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/calcs/{value1}/{operator}/{value2}', [CalcController::class, 'result']);
